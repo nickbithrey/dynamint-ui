4606ff23d908e6ff3fe49303b7e5541fe6454af8
Nick Bithrey <nick.bithrey@sword-apak.com>
2019-06-14 11:56:41 +0100

    move test from model to model selection test pages

--- test/model/selection/ModelSelection.test.js
+++ test/model/selection/ModelSelection.test.js
@@ -19,5 +19,25 @@ describe('<ModelSelection /> rendering', () => {
 		
 		expect(wrapper).toMatchSnapshot();
 	});
+		
+//		it('renders create button', () => {
+//		const load = jest.fn(() => []);
+//		const create = jest.fn(() => []);
+//		const models = {
+//			models: null
+//		}
+//		const wrapper = shallow( <Model load={load} create={create} models={models} /> );
+//		
+//		const link = wrapper.find(Link);
+//		expect(link).toHaveLength(1);
+//		expect(link.children().text()).toBe('Create');
+//		expect(link.prop('to')).toBe('/models/create');
+//		expect(link.prop('replace')).toBeFalsy();
+//		
+//		const route = wrapper.find(Route);
+//		expect(route).toHaveLength(1);
+//		expect(route.children().length).toBe(0);
+//		expect(route.prop('path')).toBe('/models/:type');
+//	});
 	
 });
