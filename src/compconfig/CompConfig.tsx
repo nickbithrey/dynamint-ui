863ef51c1a1132c98157a7810959fb883140fc01
Nick Bithrey <nickbithrey@gmail.com>
2019-10-12 23:08:45 +0100

    Update CompConfig.tsx

--- src/compconfig/CompConfig.tsx
+++ src/compconfig/CompConfig.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Link } from 'react-router-dom';
 import Selection from './selection';
 import Details from './details';
 
@@ -7,6 +7,7 @@ const CompConfig = () => (
     <Switch>
         <Route exact path={'/compconfig'} component={Selection} />
         <Route exact path={'/compconfig/details'} component={Details} />
+        <Link to="/compconfig">Config</Link>
     </Switch>
 );
 
